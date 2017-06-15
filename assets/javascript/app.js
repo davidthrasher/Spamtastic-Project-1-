a3dddae212a756de95bf1d3a2514be4c871f4a57
Harrison McCord <harrison.mccord@gmail.com>
2017-06-15 18:26:04 -0400

    init

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPoB3vEaH9uWLWw0q-eXGIQEgNvU23uJs",
+    authDomain: "spamtastic-255ab.firebaseapp.com",
+    databaseURL: "https://spamtastic-255ab.firebaseio.com",
+    projectId: "spamtastic-255ab",
+    storageBucket: "",
+    messagingSenderId: "851277849763"
+  };
+  firebase.initializeApp(config);
