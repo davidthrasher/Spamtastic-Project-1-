21937475938a89f008a47458e684d949dfcc0f8c
David Thrasher <thrasher1332@hotmail.com>
2017-06-17 13:58:17 -0400

    updated JS2,Need IF/Else statement

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,26 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBPoB3vEaH9uWLWw0q-eXGIQEgNvU23uJs",
-    authDomain: "spamtastic-255ab.firebaseapp.com",
-    databaseURL: "https://spamtastic-255ab.firebaseio.com",
-    projectId: "spamtastic-255ab",
-    storageBucket: "",
-    messagingSenderId: "851277849763"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyBPoB3vEaH9uWLWw0q-eXGIQEgNvU23uJs",
+  authDomain: "spamtastic-255ab.firebaseapp.com",
+  databaseURL: "https://spamtastic-255ab.firebaseio.com",
+  projectId: "spamtastic-255ab",
+  storageBucket: "",
+  messagingSenderId: "851277849763"
+};
+firebase.initializeApp(config);
+
+// research axios
 
 //Establish Global Variables
 var database = firebase.database();
-
 var userNumber = "";
-
 var reverseKey = "9608ae9a2c4d4ff89e6e11b6c07304ba";
-
 var reputationKey = "717e3976df9a4ddba7f5cf0f139ba0d2";
-
 var mapsKey = "AIzaSyCsptyS96_W0OHNgvk792B6ASpVNdM6tqA";
+
+var retrievedPerson = {
+  fullName: ''
+}
 
 function makeRequest(url, cb) {
   $.ajax({
@@ -31,21 +33,27 @@ function makeRequest(url, cb) {
   });
 }
 
-function writePhoneInfo(object) {
+function writeReverseInfo(object) {
+  //Need an If/Else statement to provide placeholder name in the event that no name is available from API.
   var person = object.belongs_to[0].firstname + ' ' + object.belongs_to[0].lastname;
-  console.log(person);
   var carrier = object.carrier;
-  console.log(carrier);
-  var reputation = object.reputation_level;
-  console.log(reputation);
-  var callType = object.reputation_details;
-  console.log(callType);
-  var location = object.current_addresses[0].lat_long[1,2];
-  console.log(location);
+  var lat = object.current_addresses.object.lat_long.latitude;
+  var lng = object.current_addresses.object.lat_long.longitude;
+  console.log(lat);
+  console.log(lng);
 
   var $row = $('<tr>');
   $row.append($('<td>').text(person));
   $row.append($('<td>').text(carrier));
+  $('#number-info').append($row);
+}
+
+function writeReputationInfo(object) {
+  var reputation = object.reputation_level;
+  var callType = object.reputation_details.type;
+  console.log(callType);
+
+  var $row = $('<tr>');
   $row.append($('<td>').text(reputation));
   $row.append($('<td>').text(callType));
   $('#number-info').append($row);
@@ -55,16 +63,16 @@ function writePhoneInfo(object) {
 $("#add-number-btn").on("click", function(event) {
     event.preventDefault();
 
+    $("#number-info").empty();
+
     userNumber = $("#number-input").val().trim();
 
     var queryURLReverse = "https://proapi.whitepages.com/3.0/phone?phone=" + userNumber + "&api_key=" + reverseKey;
 
     var queryURLReputation = "https://proapi.whitepages.com/3.0/phone_reputation?phone=" + userNumber + "&api_key=" + reputationKey;
 
-    console.log(queryURLReverse);
-    console.log(queryURLReputation);
+    makeRequest(queryURLReverse, writeReverseInfo);
+    makeRequest(queryURLReputation, writeReputationInfo);
 
-    makeRequest(queryURLReverse, writePhoneInfo);
-    makeRequest(queryURLReputation, writePhoneInfo);
 
 });
