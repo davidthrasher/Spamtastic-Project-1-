cffa28e883f41c0f24fcf6d3db38cac306bee123
David Thrasher <thrasher1332@hotmail.com>
2017-06-22 18:55:04 -0400

    axios update 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 // Initialize Firebase
+// globals: axios
+
 var config = {
   apiKey: "AIzaSyBPoB3vEaH9uWLWw0q-eXGIQEgNvU23uJs",
   authDomain: "spamtastic-255ab.firebaseapp.com",
@@ -14,35 +16,27 @@ firebase.initializeApp(config);
 //Establish Global Variables
 var database = firebase.database();
 var userNumber = "";
-var reverseKey = "9608ae9a2c4d4ff89e6e11b6c07304ba";
-var reputationKey = "717e3976df9a4ddba7f5cf0f139ba0d2";
+var reverseKey = "19a8f98d256c43c8b370ddf3a30130b9";
+var reputationKey = "6112b420059f4d7c81cf99d5378dc961";
 var mapsKey = "AIzaSyCsptyS96_W0OHNgvk792B6ASpVNdM6tqA";
-
-var retrievedPerson = {
-  fullName: ''
-}
-
-function makeRequest(url, cb) {
-  $.ajax({
-    url: url,
-    method: "GET"
-  })
-  .done(function(response){
-    console.log(response);
-    cb(response);
-  });
-}
 
 function writeReputationInfo(object) {
   var reputation = object.reputation_level;
-  var callType = object.reputation_details.type;
-  console.log(reputation);
-  console.log(callType);
+  var callType = "";
+  if (object.reputation_details.category = "null") {
+    callType = object.reputation_details.type;
+  } else {
+    callType = object.reputation_details.category;
+  }
 
-  var $row = $('<tr>');
-  $row.append($('<td>').text(reputation));
-  $row.append($('<td>').text(callType));
-  $('#number-info').append($row);
+  console.log("===== Reputation: " + reputation);
+  console.log("===== Call Type: " + callType);
+
+  return {
+    reputation: reputation,
+    callType: callType
+  }
+
 }
 
 function writeReverseInfo(object) {
@@ -50,36 +44,22 @@ function writeReverseInfo(object) {
 
     var person = "";
     var carrier = "";
-    if (object.belongs_to[0].firstname && object.belongs_to[0].lastname) {
+
+    if (object.belongs_to.length) {
       person = object.belongs_to[0].firstname + ' ' + object.belongs_to[0].lastname;
-    } else if (object.belongs_to[0].firstname) {
-      person = object.belongs_to[0].firstname;
-    } else if (object.belongs_to[0].lastname) {
-      person = object.belongs_to[0].lastname;
     } else {
       person = "Not Available";
     }
 
-    if (object.carrier) {
-      carrier = object.carrier;
-    } else {
-      carrier = "Not Available";
-    }
 
-
-    var person = object.belongs_to[0].firstname + ' ' + object.belongs_to[0].lastname;
+    // var person = object.belongs_to[0].firstname + ' ' + object.belongs_to[0].lastname;
     var carrier = object.carrier;
-    console.log(person);
-    console.log(carrier);
+    console.log("===== Caller: " + person);
+    console.log("===== Carrier: " + carrier);
     var lat = object.current_addresses[0].lat_long.latitude;
     var lng = object.current_addresses[0].lat_long.longitude;
     console.log(lat);
-    console.log(lng);
-
-    var $row = $('<tr>');
-    $row.append($('<td>').text(person));
-    $row.append($('<td>').text(carrier));
-    $('#number-info').append($row);
+    console.log(lng);``
 
   //empy map div
   // $( "#map" ).empty();
@@ -100,6 +80,11 @@ function writeReverseInfo(object) {
         });
   }
 
+  return {
+    person: person,
+    carrier: carrier
+  }
+
 }
 
 
@@ -113,10 +98,18 @@ $("#add-number-btn").on("click", function(event) {
     userNumber = $("#number-input").val().trim();
 
     var queryURLReverse = "https://proapi.whitepages.com/3.0/phone?phone=" + userNumber + "&api_key=" + reverseKey;
-
     var queryURLReputation = "https://proapi.whitepages.com/3.0/phone_reputation?phone=" + userNumber + "&api_key=" + reputationKey;
 
-    makeRequest(queryURLReverse, writeReverseInfo);
-    makeRequest(queryURLReputation, writeReputationInfo);
-});
+    axios.all([axios.get(queryURLReverse), axios.get(queryURLReputation)])
+      .then(axios.spread(function(revLookup, reputation) {
+        reputation = writeReputationInfo(reputation);
+        revLookup = writeReverseInfo(revLookup)
 
+        var $row = $('<tr>');
+        $row.append($('<td>').text(revLookup.person));
+        $row.append($('<td>').text(revLookup.carrier));
+        $row.append($('<td>').text(reputation.reputation));
+        $row.append($('<td>').text(reputation.callType));
+        $('#number-info').append($row);
+      }))
+});
